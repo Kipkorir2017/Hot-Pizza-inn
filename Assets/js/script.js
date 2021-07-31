0fcadd045473492398c38c075136fb462677ec83
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-31 11:19:04 +0300

    add js validation for the second form

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,25 @@
 function validatePizza(){
 if (formName.fieldArea.value==""){
-    alert("Select your Pizza flavour")
+    alert("Kindly make a complete order first!")
     return false;
 }
 
+}
+
+function validateLocation(){
+    var name=document.getElementById("name").value;
+    var location=document.getElementById("location").value;
+    var phoneNumber=document.getElementById("phoneNumber").value;
+    if(name=="" || name.length<3){
+        alert("Provide a valid name:");
+        return false;
+    }
+    else if(phoneNumber=="" || phoneNumber.length<10){
+        alert("provide a valid Phone Number")
+    }
+    else if(location=="" || location.length<3){
+        alert("Provide a valid location");
+        return false;
+    }
+   
 }
