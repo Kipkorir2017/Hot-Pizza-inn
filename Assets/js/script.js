e10d7587545432da0b8921e7818b6365e291f8eb
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-31 17:43:51 +0300

    add welcome message on nav bar and also add constructor to return prices

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -32,6 +32,113 @@ function MakeOrder(type,size,crust,topping,quantity){
     this.topping=topping;
     this.quantity=quantity;
 }
+// returning the prices of crust
+MakeOrder.prototype.crust=function(){
+    if(this.crust==1){
+        return 100;
+    }
+    if(this.crust==2){
+        return 80;
+    }
+    if(this.crust==3){
+        return 150;
+    }
+    if(this.crust==4){
+        return 180;
+    }
+}
+// returning the prices of toppings
+MakeOrder.prototype.topping=function(){
+    if(this.topping==1){
+        return 100;
+    }
+    if(this.topping==2){
+        return 120
+    }
+    if(this.topping==3){
+        return 150;
+    }
+    if(this.topping==4){
+        return 180;
+    }
+    if(this.topping==5){
+        return 200;
+    }
+    if(this.topping==6){
+        return 250;
+    }
+
+}
+// returning the price  of Pizza 
+MakeOrder.prototype.type=function(){
+    if(this.type==1){
+        if(this.size=1){
+            return 600;
+        }
+        else if(this.size==2){
+            return 900;
+        }
+        else if(this.size==3){
+            return 1200;
+        }
+
+    }
+    else if(this.type==2){
+        if(this.size==1){
+            return 550;
+        } 
+        else if(this.size==2){
+            return 650;
+        }
+        else if(this.size==3){
+            return 1250;
+        }
+    }
+    else if(this.type==3){
+        if(this.size=1){
+            return 600;
+        }
+        else if(this.size==2){
+            return 750;
+        }
+        else if(this.size==3){
+            return 1450;
+        }
+    }
+    else if(this.type==4){
+        if(this.size==1){
+            return 400
+        }
+        else if(this.size==2){
+            return 800;
+        }
+        else if(this.size==3){
+            return 1600
+        }
+    }
+    else if(this.type==5){
+        if(this.size==1){
+            return 350;
+        }
+        else if(this.size==2){
+            return 700;
+        }
+        else if(this.size==3){
+            return 1400;
+        }
+    }
+    else if(this.type==6){
+        if(this.size==1){
+            return 300;
+        }
+        else  if(this.size==2){
+            return 750;
+        }
+        else if(this.size==3){
+            return 1450;
+        }
+    }
+}
 
 
 
